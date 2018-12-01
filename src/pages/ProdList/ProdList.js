5946dda4e61f5463974ab1380b07e792fc7f47ff
Emrick L <esppk@outlook.com>
2018-12-02 03:31:26 +0800

    add pagination

--- src/pages/ProdList/ProdList.js
+++ src/pages/ProdList/ProdList.js
@@ -3,12 +3,27 @@ import Card from "../../extraComponents/Card";
 import "./prodList.css";
 
 class ProdList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      curPage: 1
+    };
+    this.goPage = this.goPage.bind(this);
+  }
+
   async componentDidMount() {
     let prods = await this.props.products;
     console.log(prods);
   }
 
+  goPage(page) {
+    this.setState({ curPage: page });
+  }
+
   render() {
+    const len = this.props.products.length;
+    let pages = Math.ceil(len / 12);
+
     return (
       <div>
         <h1>ProdList</h1>
@@ -16,9 +31,26 @@ class ProdList extends Component {
           <div className="sidebar">siderbar function</div>
 
           <div className="prodGrid">
-            {this.props.products.slice(0, 10).map(prod => {
-              return <Card name={prod.name} imagelink={prod.imagelink} />;
-            })}
+            {this.props.products
+              .slice((this.state.curPage - 1) * 12, this.state.curPage * 12)
+              .map(prod => {
+                return <Card name={prod.name} imagelink={prod.imagelink} />;
+              })}
+          </div>
+          <div className="pageControl">
+            {Array(pages)
+              .fill(1)
+              .map((x, y) => {
+                let numClass = "pageNum";
+                if (x + y === this.state.curPage) {
+                  numClass = "pageNum active";
+                }
+                return (
+                  <span className={numClass} onClick={() => this.goPage(x + y)}>
+                    {x + y}
+                  </span>
+                );
+              })}
           </div>
         </div>
       </div>
